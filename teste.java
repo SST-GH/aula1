069eff824871c21b69228d1c85db01d6f52b8b42
studentsara <97111715+studentsara@users.noreply.github.com>
2022-01-04 15:38:54 +0000

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,14 @@
+public class Teste {
+  public static void main {
+    Scanner dadosUt = new Scanner(System.in);
+    System.out.println("Insira o primeiro número: ");
+    int primeiroNum = dadosUt.nextInt();
+    System.out.println("Insira o segundo número: ");
+    int segundoNum = dadosUt.nextInt();
+    int soma = primeiroNum + segundoNum;
+    System.out.println("A soma é: " +soma);
+    
+    
+    
+    }
+}
